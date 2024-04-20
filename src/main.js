86b21afa4a4875b5e83eab113c348bdfb8792f4e
tejasaru04 <156053856+tejasaru04@users.noreply.github.com>
2024-04-20 23:12:17 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@ window.alert(1);
 
 let p = document.createElement("p");
 
-document.getElementsByTagName('body')[0].appendChild("javascript text", p)
+document.getElementsByTagName('body')[0].append("javascript text", p)
